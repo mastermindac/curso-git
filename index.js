8d34a889ef021e0ef06e9f6e319e95edd50f085a
Bruno Sosa <sosa.programmer@gmail.com>
2022-06-29 14:24:02 -0300

    Video pul request

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const url = "https://jsonplaceholder.typicode.com";
 
-const getUser = async (id) => {
-  const res = await fetch(`${url}/users?id=${id}`);
-  const user = (await res.json())[0];
+const getUser = async(id) => {
+    const res = await fetch(`${url}/users?id=${id}`);
+    const user = (await res.json())[0];
 
-  return user;
+    return user;
 }
 
-const getPosts = async (user) => {
-  const res = await fetch(`${url}/posts?userId=${user.id}&_limit=3`)
-  const posts = await res.json();
+const getPosts = async(user) => {
+    const res = await fetch(`${url}/posts?userId=${user.id}&_limit=3`)
+    const posts = await res.json();
 
-  return posts;
+    return posts;
 }
 
-const getCommentsForEachPost = async (posts) => {
-  const res = await Promise.all(posts.map(post => 
-    fetch(`${url}/comments?postId=${post.id}&_limit=4`)
-  ));
-  console.log(res); 
-  const postComments = await Promise.all(res.map(r => r.json()));
-  
-  postComments.forEach((comments, i) => posts[i].comments = comments);
+const getCommentsForEachPost = async(posts) => {
+    const res = await Promise.all(posts.map(post =>
+        fetch(`${url}/comments?postId=${post.id}&_limit=4`)
+    ));
+    console.log(res);
+    const postComments = await Promise.all(res.map(r => r.json()));
+
+    postComments.forEach((comments, i) => posts[i].comments = comments);
 }
 
 const renderHtml = (user, posts) => {
-  const content = document.getElementById('content');
-  content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
-  
-  posts.forEach(post => {
-    content.innerHTML += `
+        const content = document.getElementById('content');
+        content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
+
+        posts.forEach(post => {
+                    content.innerHTML += `
     <div class="post">
       <h4>${post.title}</h4>
       <p>${post.body}</p>
@@ -71,3 +71,4 @@ const affiliateRedirect = () => {
 // cambios videos 1
 // cambios videos 2
 // cambios videos 3
+// cambios videos 4
