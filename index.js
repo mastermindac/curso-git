76cac40e255bcbb3584de798ba16f2e443e1e23a
AntonioValleD <juan.valle.d1@gmail.com>
2023-04-19 12:29:31 -0600

    Max num function added

--- index.js
+++ index.js
@@ -62,12 +62,12 @@ const affiliateRedirect = () => {
   // resolver el bug
 }
 
-// test commit
-
-// cambio 1
-// cambio 2
-// cambio 3
-
-// cambios videos 1
-// cambios videos 2
-// cambios videos 3
+const maxValue = (nums) => {
+    let maxNum = nums[0];
+    for (let num of nums){
+      if (maxNum < num){
+        maxNum = num;
+      }
+    }
+    return maxNum;
+};
