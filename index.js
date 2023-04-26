a5183fb8cff6bfd4a8b2aeaac3e8330946bd7d65
Angel-Giovanni <daqkumark@gmail.com>
2023-04-25 22:51:17 -0400

    Cambios backend

--- index.js
+++ index.js
@@ -64,10 +64,6 @@ const affiliateRedirect = () => {
 
 // test commit
 
-// cambio 1
-// cambio 2
-// cambio 3
-
-// cambios videos 1
-// cambios videos 2
-// cambios videos 3
+// Cambio número 1
+// Cambio número 2
+// Cambio número 3
