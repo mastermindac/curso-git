9320ffffa22d34bab61ddca7603f4dd8b3a7166e
Uriel Varela <urielvarela560@gmail.com>
2023-08-24 23:52:15 -0400

    update uri commnts

--- index.js
+++ index.js
@@ -67,9 +67,9 @@ const affiliateRedirect = () => {
 // cambio 1
 // cambio 2
 // cambio 3
-//cambio 4
+// cambio 4
 
 // cambios videos 1
 // cambios videos 2
 // cambios videos 3
-//cambios videos 4 a
+// cambios videos 4 a
